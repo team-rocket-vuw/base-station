63860485ece8d91520aa212f4735885e144ec188
Recursively <Marcel.vanWorkum@gmail.com>
2016-09-13 17:20:56 +1200

    Implement Baseline GUI structure
    Created a baseline GUI layout which uses three seperate tabs to
    display the different components of the GUI window.
    
    Implemented the one of the three tabs, the 'Rocket Location' tab
    which shows the rockets current location using a Google Static Map
    as well as the fetch time, and the option to re-fetch the location
    map.
    
    Added placeholder classes for the control and status tabs.
    
    Refactored all documentation to fit the standard python conventions
    
    Refactored project directory strucutre to be more modular

--- software/util/map_downloader.py
+++ software/util/map_downloader.py
@@ -1,42 +1,44 @@
 import requests
 import os
 
-"""
-Helper class to download a small hybrid Google map, with a marker
-at at a specified lat-long coordinate. The file is then saved to a
-specified {filename}.png
 
-Created 07 September 2016
-By Marcel van Workum
-
-Modified 07 September 2016
-By Marcel van Workum
-"""
 class MapDownloader:
+    """
+    Helper class to download a small hybrid Google map, with a marker
+    at at a specified lat-long coordinate. The file is then saved to a
+    specified {filename}
+
+    Created 07 September 2016
+    By Marcel van Workum
+
+    Modified 13 September 2016
+    By Marcel van Workum
+    """
+
     API_ROOT = "https://maps.googleapis.com/maps/api/staticmap?"
     API_ID = "google_maps_api_key"
     API_SEPARATOR = "&"
-    SIZE = "size=400x400"
+    SIZE = "size=700x700"
     ZOOM = "zoom=16"
-    MAP_TYPE = "hybrid"
+    MAP_TYPE = "maptype=hybrid"
     MARKER_TEMPLATE = "markers=color:red%7Clabel:R%7C"
 
-    """
-    initialises the MapDownloader taking in a location, which should be a comma separated
-    "lat, long" string, and a filename.
-
-    Example use:
-
-     ` MapDownloader("-41.2880647,174.7617035", "example") `
-
-    """
     def __init__(self, location, filename):
+        """
+        Initialises the MapDownloader taking in a location, which should be a comma separated
+        "lat, long" string, and a filename.
+
+        Example use:
+
+         ` MapDownloader("-41.2880647,174.7617035", "example") `
+
+        """
         # initialize the location and center of map, setting a mark at the location
         self.location = location
         self.center = "center=" + location
         self.marker = self.MARKER_TEMPLATE + location
 
-        # filename to save the resulting png map to
+        # filename to save the resulting image map to
         self.filename = filename
 
         # create the request session, used to download the map
@@ -46,21 +48,21 @@ class MapDownloader:
         self.api_key = os.environ[self.API_ID]
 
     def download_map(self):
-        print("hello")
+        """
+        Constructs the map download url and then uses the session to download the image data, saving
+         it to the specified file
+        """
         # construct the request url
         request = self.API_ROOT + self.center + "&" + self.ZOOM + \
                   "&" + self.SIZE + "&" + self.MAP_TYPE + \
                   "&" + self.marker + "&" + self.api_key
 
-        print(request)
-
         # get the map from Google API
         response = self.session.get(request)
 
         # open a new file with the specified filename and write the image to that file
-        f = open(self.filename + ".png", 'wb')
+        f = open(self.filename, 'wb')
         f.write(response.content)
 
         # finally close the file
         f.close()
-
