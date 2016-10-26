c014570ff992ac1c63e1dfcdc3da72d623ae617f
Recursively <Marcel.vanWorkum@gmail.com>
2016-10-26 21:52:08 +1300

    --wip--

--- software/src/static/scripts/jsx/simulationContainer.jsx
+++ software/src/static/scripts/jsx/simulationContainer.jsx
@@ -1,6 +1,64 @@
 import React from 'react';
 import $ from "jquery";
 
+const LineChart = require("react-chartjs").Line;
+
+var SimulationBarChart = React.createClass({
+  render: function() {
+    var labels = [];
+    var maxAltitudes = [];
+    var maxVelocities = [];
+
+    for(var launch in this.props.data) {
+      labels.push(launch);
+      maxAltitudes.push(this.props.data[launch]["launchStatistics"]["maxAltitude"]);
+      maxVelocities.push(this.props.data[launch]["launchStatistics"]["maxVelocity"]);
+    }
+
+    var data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "Max Altitudes",
+          fillColor: "transparent",
+          strokeColor: "black",
+          pointColor: "black",
+          pointStrokeColor: "black",
+          pointHighlightFill: "black",
+          pointHighlightStroke: "black",
+          yAxisID: "maxAltitudeAxis",
+          data: maxAltitudes
+        },
+        {
+          label: "Max Velocities",
+          fillColor: "transparent",
+          strokeColor: "red",
+          pointColor: "red",
+          pointStrokeColor: "red",
+          pointHighlightFill: "red",
+          pointHighlightStroke: "red",
+          yAxisID: "maxVelocityAxis",
+          data: maxVelocities
+        },
+      ]
+    };
+
+    var options = {
+      scales: {
+        yAxes: [{
+          position: "left",
+          "id": "maxAltitudeAxis"
+        }, {
+          position: "right",
+          "id": "maxVelocityAxis"
+        }]
+      }
+    }
+
+    return <LineChart data={data} options={options} width="800" height="600" />
+  }
+});
+
 var Launch = React.createClass({
   launchAngle: function() {
     return <h4 className="launch-angle">Launch Angle: {this.props.data["launchAngle"]} </h4>;
@@ -44,6 +102,7 @@ var Simulation = React.createClass({
     return(
       <div className="simulation">
         <h3 className="simulation-name">Simulation Name: {this.props.name}</h3>
+        <SimulationBarChart data={this.props.data} />
         {this.formattedSimulationData()}
       </div>
     );
