69472b9625d5b7ba3df9c988b6655f19ad78735a
Recursively <Marcel.vanWorkum@gmail.com>
2016-10-27 22:54:20 +1300

    add animations to loading

--- software/src/static/scripts/jsx/controlsContainer.jsx
+++ software/src/static/scripts/jsx/controlsContainer.jsx
@@ -3,7 +3,11 @@ import $ from "jquery";
 
 var ControlsContainer = React.createClass({
   getInitialState: function() {
-    return({});
+    return({
+      displayStartButton: false,
+      displaySkipButton: false,
+      displayBeginButton: false
+    });
   },
 
   sendCommand: function(command) {
@@ -37,14 +41,90 @@ var ControlsContainer = React.createClass({
     }
   },
 
-  render: function() {
+  renderStateDisplay: function() {
     var rocketState = this.props.data.responseData.rocket_state
+
+    var rfmStateClass = "loading";
+    var rfmStateContent = "";
+    switch(rocketState.init_info.RFM) {
+      case "True":
+        rfmStateClass = "loaded";
+        rfmStateContent = "✔";
+      break;
+      case "False":
+        rfmStateClass = "fail";
+        rfmStateClass = "✖"
+      break;
+    }
+
+    var dmStateClass = "loading";
+    var dmStateContent = "";
+    switch(rocketState.init_info.DM) {
+      case "True":
+        dmStateClass = "loaded";
+        dmStateContent = "✔";
+      break;
+      case "False":
+        dmStateClass = "fail";
+        dmStateContent = "✖";
+      break;
+    }
+
+    var gpsStateClass = "loading";
+    var gpsStateContent = "";
+    switch(rocketState.gps_info.STATE) {
+      case "waiting":
+        gpsStateClass = "waiting";
+        gpsStateContent = "•";
+      break;
+      case "skipped":
+        gpsStateClass = "fail";
+        gpsStateContent = "✖";
+      break;
+      case "ready":
+        gpsStateClass = "loaded";
+        gpsStateContent = "✔";
+      break;
+    }
+
+    return(
+      <div className="state-display">
+        <h3>Rocket Initialisation Info</h3>
+        <div>
+          <div className={rfmStateClass}>{rfmStateContent}</div><span className="rfm-state">Radio Module: {rocketState.init_info.RFM}</span>
+        </div>
+        <div>
+          <div className={dmStateClass}>{dmStateContent}</div><span>Data Module: {rocketState.init_info.DM}</span>
+        </div>
+
+        <h3>GPS State</h3>
+        <div className={gpsStateClass} /><span>Status: {rocketState.gps_info.STATE}</span>
+        <div>Visible Satellites: {parseInt(rocketState.gps_info.VIS)}</div>
+      </div>
+    );
+  },
+
+  renderButtons: function() {
+    var buttons = [];
+
+    if (this.state.displaySkipButton) {
+      buttons.push(<button className="control-button" onClick={() => this.sendCommand("start")}>Send Start Command</button>);
+    }
+    if (this.state.displaySkipButton) {
+      buttons.push(<button className="control-button" onClick={() => this.sendCommand("skip")}>Send Skip Command</button>);
+    }
+    if (this.state.displaySkipButton) {
+      buttons.push(<button className="control-button" onClick={() => this.sendCommand("begin")}>Send Begin Command</button>);
+    }
+
+    return buttons;
+  },
+
+  render: function() {
     return(
       <div className="controls-container">
-        <p>{rocketState.init_info.RFM}</p>
-        <button className="control-button" onClick={() => this.sendCommand("start")}>Send Start Command</button>
-        <button className="control-button" onClick={() => this.sendCommand("skip")}>Send Skip Command</button>
-        <button className="control-button" onClick={() => this.sendCommand("begin")}>Send Begin Command</button>
+        {this.renderStateDisplay()}
+        {this.renderButtons()}
       </div>
     );
   }
